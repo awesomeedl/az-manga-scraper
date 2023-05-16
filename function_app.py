bad4b5d55a792332e0d90ea437851d00dd5b7560
awesomeedl <82195917+awesomeedl@users.noreply.github.com>
2023-05-16 16:27:27 +0800

    initial version

---
+++ function_app.py
@@ -0,0 +1,31 @@
+import datetime
+import azure.functions as func
+import logging
+from bs4 import BeautifulSoup
+import requests
+
+app = func.FunctionApp()
+
+# Learn more at aka.ms/pythonprogrammingmodel
+
+@app.function_name(name="mytimer")
+@app.schedule(schedule="0 */5 * * * *", arg_name="mytimer", run_on_startup=True,
+              use_monitor=False) 
+def test_function(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+
+    if mytimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+
+    url = 'https://www.manhuagui.com/comic/31550/'
+    result = requests.get(url)
+
+    soup = BeautifulSoup(result.content, 'html.parser')
+
+    logging.info(soup)
+
+
+
