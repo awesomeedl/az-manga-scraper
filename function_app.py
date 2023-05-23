97c791c08fa356b683866236c94c151320a6f75f
awesomeedl <82195917+awesomeedl@users.noreply.github.com>
2023-05-23 16:47:27 +0800

    progress

--- function_app.py
+++ function_app.py
@@ -1,92 +1,101 @@
 import datetime
-import azure.functions as func
-import azure.durable_functions as df
-from azure.data.tables import TableClient
 import logging
-from bs4 import BeautifulSoup
-import requests
 import os
+
+import azure.durable_functions as df
+import azure.functions as func
+import requests
+from azure.data.tables import TableClient
+from bs4 import BeautifulSoup
 
 # Learn more at aka.ms/pythonprogrammingmodel
 
-# @app.function_name(name="mytimer")
-# @app.schedule(schedule="0 */5 * * * *", arg_name="mytimer", run_on_startup=True,
-#               use_monitor=False) 
-# def test_function(mytimer: func.TimerRequest) -> None:
-#     utc_timestamp = datetime.datetime.utcnow().replace(
-#         tzinfo=datetime.timezone.utc).isoformat()
-
-#     if mytimer.past_due:
-#         logging.info('The timer is past due!')
-
-#     logging.info('Python timer trigger function ran at %s', utc_timestamp)
-
-#     url = 'https://www.manhuagui.com/comic/31550/'
-#     result = requests.get(url)
-
-#     soup = BeautifulSoup(result.content, 'html.parser')
-#     filtered = '\n'.join(map(str, soup.find_all('a', {'class': 'status0'})))
-
-#     logging.info(filtered)
-
-
 app = df.DFApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.durable_client_input(client_name="client")
-@app.schedule(schedule="0 */5 * * * *", arg_name="mytimer", run_on_startup=True,
-              use_monitor=False) 
-async def timer_start(mytimer: func.TimerRequest, client):
-    utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
-    if mytimer.past_due:
-        logging.info('The timer is past due!')
+base_url = "https://www.manhuagui.com/comic/"
 
-    instance_id = await client.start_new("hello_orchestrator")
-    logging.info(f'Python timer trigger function ran at {utc_timestamp}, instance id = {instance_id}')
+
+@app.durable_client_input(client_name="client")
+@app.schedule(
+    schedule="0 */5 * * * *", arg_name="mytimer", run_on_startup=True, use_monitor=False
+)
+async def timer_start(mytimer: func.TimerRequest, client: df.DurableOrchestrationClient):
+    utc_timestamp = (
+        datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+    )
+    if mytimer.past_due:
+        logging.info("The timer is past due!")
+
+    instance_id = await client.start_new("scrape_orchestrator")
+    
+    logging.info(
+        f"Python timer trigger function ran at {utc_timestamp}, instance id = {instance_id}"
+    )
+
 
 # Orchestrator
 @app.orchestration_trigger(context_name="context")
-def hello_orchestrator(context):
+def scrape_orchestrator(context: df.DurableOrchestrationContext):
+    manga_list = yield context.call_activity("generate_list")
+
+    tasks = [context.call_activity("scrape", manga) for manga in manga_list.items()]
+
+    results: list[dict] = yield context.task_all(tasks)
+
+    yield context.call_activity("notify", results)
+
+@app.activity_trigger(input_name="param")
+def generate_list(param):
     table_client = TableClient.from_connection_string(
-        conn_str=os.environ["AzureWebJobsStorage"],
-        table_name="manga"
+        conn_str=os.environ["AzureWebJobsStorage"], table_name="manga"
     )
 
     entities = table_client.query_entities("PartitionKey eq 'index'")
 
-    ids = [entity['RowKey'] for entity in entities]
+    return { int(entity["RowKey"]): int(entity["latest"]) if entity.get("latest") else None for entity in entities }
 
-    tasks = [context.call_activity("scrape", id) for id in ids]
+@app.activity_trigger(input_name="manga")
+def scrape(manga: tuple[int: int | None]):
+    logging.info(f"task received manga {manga}")
 
-    logging.info((yield context.task_all(tasks)))
-    
-# An HTTP-Triggered Function with a Durable Functions Client binding
-# @app.route(route="orchestrators/{functionName}")
-# @app.durable_client_input(client_name="client")
-# async def http_start(req: func.HttpRequest, client):
-#     function_name = req.route_params.get('functionName')
-#     instance_id = await client.start_new(function_name)
-#     response = client.create_check_status_response(req, instance_id)
-#     return response
+    manga_id, latest_ep = manga[0], manga[1]
 
-# # Orchestrator
-# @app.orchestration_trigger(context_name="context")
-# def hello_orchestrator(context):
-#     ids = [1, 2, 3]
+    result = requests.get(f"{base_url}{manga_id}/")
 
-#     tasks = [context.call_activity("scrape", id) for id in ids]
+    soup = BeautifulSoup(result.content, "html.parser")
 
-#     return (yield context.task_all(tasks))
+    manga_episodes = {}
 
-# Activity
-@app.activity_trigger(input_name="mangaid")
-def scrape(mangaid: int):
-    logging.info(f"task received mangaid {mangaid}")
-    base_url = 'https://www.manhuagui.com/comic/'
+    for a in soup.find_all("a", class_="status0"):
+        # This following statement extracts the episode id,
+        # i.e. 574591 out of the href link
+        # href="/comic/37456/574591.html"
+        episode_id = int(a["href"].split("/")[-1].split(".")[0])
+        manga_episodes[episode_id] = a["title"]
 
-    result = requests.get(f'{base_url}{mangaid}/')
+    if latest_ep:
+        manga_episodes = {k: v for k, v in manga_episodes.items() if k > latest_ep}
+        return manga_episodes
+    else:
+        latest = max(manga_episodes.keys())
+        return {latest: manga_episodes[latest]}
 
-    soup = BeautifulSoup(result.content, 'html.parser')
-    return str(soup.find_all('a', {'class': 'status0'})[0])
+@app.activity_trigger(input_name="new_manga_list")
+def notify(new_manga_list: list[dict]):
+    table_client = TableClient.from_connection_string(
+        conn_str=os.environ["AzureWebJobsStorage"], table_name="manga"
+    )
 
+    notify_url = os.environ["NotifyURL"]
 
+    # logging.info(results)
+    for manga in new_manga_list:
+        for manga_id, latest_ep in manga.items():
+            entity = table_client.get_entity('index', str(manga_id))
+            if entity.get('latest'):
+                body = { "content": "new manga notification" }
+                requests.post(notify_url, json=body )
+
+                entity['latest'] = latest_ep
+
+                table_client.update_entity()
